000d75003afad9e9850f12093a7b4d220c209061
HoanhVu72 <vhoanh306@gmail.com>
2025-11-13 23:19:16 +0700

    Add files via upload

---
+++ bai5.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long r = 10;
+    double ChuViHinhTron = 2 * r * 3.14;
+    double DienTichHinhTron = r * r * 3.14;
+    cout << fixed << setprecision(2) << ChuViHinhTron << "\n" << fixed << setprecision(2) << DienTichHinhTron << "\n";
+}
