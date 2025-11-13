9a0caff8c624a041a095d1c32d143f3b0ca735f5
HoanhVu72 <vhoanh306@gmail.com>
2025-11-13 23:36:34 +0700

    Delete bai5.cpp

--- bai5.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    long long r = 10;
-    double ChuViHinhTron = 2 * r * 3.14;
-    double DienTichHinhTron = r * r * 3.14;
-    cout << fixed << setprecision(2) << ChuViHinhTron << "\n" << fixed << setprecision(2) << DienTichHinhTron << "\n";
-}
