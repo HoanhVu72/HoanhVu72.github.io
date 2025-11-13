daa67ecceba929d5c727e0b26bd5a2d4e9d088eb
HoanhVu72 <vhoanh306@gmail.com>
2025-11-13 23:13:21 +0700

    Add files via upload

---
+++ bai3.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long a, b, c;
+    double ((1 <= a) && (a < 1e9));
+    double ((1 <= b) && (b < 1e9));
+    double ((1 <= c) && (c < 1e9));
+    cin >> a >> b >> c;
+    long long S = a*(b+c) + b*(a+c);
+    cout << "S = " << S << "\n";
+}
